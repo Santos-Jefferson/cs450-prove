2997f4e74babdee9ed323442cd7bb87424ee80a5
Santos-Jefferson <jefferson2004@gmail.com>
2018-01-20 01:39:09 -0700

    Prove 02 - KNN from scratch with PLOT and KNN algo

--- cs450-proves/prove02.py
+++ cs450-proves/prove02.py
@@ -1,29 +1,16 @@
 # PART 1
-import inline as inline
-import matplotlib
+from collections import Counter
+from operator import itemgetter
 import numpy as np
 from sklearn import datasets, metrics
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
+import math
+
 
 iris = datasets.load_iris()
 
-# Show the data (the attributes of each instance)
-print("Printing Data (Called Features)")
-print(iris.data)
-
-print("\nName of the features")
-print(iris.feature_names)
-
-# Show the target values (in numeric format) of each instance
-print("\nPrinting Targets")
-print(iris.target)
-
-# Show the actual target names that correspond to each number
-print("\nPrinting TargetNames")
-print(iris.target_names)
 
 # PART 2
 Xdata = iris.data
@@ -31,86 +18,54 @@ ytarget = iris.target
 
 # Using train_test_split (30% test, 70% train)
 data_train, data_test, targets_train, targets_test = train_test_split(Xdata, ytarget, test_size=0.3)
-print(data_train.shape, targets_train.shape)
-print(data_test.shape, targets_test.shape)
+# print(data_train.shape, targets_train.shape)
 
-# PART 3
-classifier = GaussianNB()
-model = classifier.fit(data_train, targets_train)
+# reformat train/test datasets for convenience
+train = np.array(list(zip(data_train, targets_train)))
+test = np.array(list(zip(data_test, targets_test)))
 
-# PART 4
-targets_predicted = classifier.predict(data_test)
-print("\nPrinting GaussianNB Classifier")
-print(targets_predicted)
-print(targets_test)
-comparison = metrics.accuracy_score(targets_test, targets_predicted)
-print("{:.3f}%".format(comparison * 100))
+# Array of predictions
+arrayPredictions = []
+
+# number of K
+k = 5
 
 
-# PART 5
-class HardCodedModel:
-    def __init__(self):
-        pass
-
-    def predict(self, data):
-        targets = []
-
-        for row in data:
-            targets.append(self.predict_one(row))
-
-        return targets
+def get_neighbours(training_set, test_instance, k):
+    distances = [_get_tuple_distance(training_instance, test_instance) for training_instance in training_set]
+    # index 1 is the calculated distance between training_instance and test_instance
+    sorted_distances = sorted(distances, key=itemgetter(1))
+    # extract only training instances
+    sorted_training_instances = [tuple[0] for tuple in sorted_distances]
+    # select first k elements
+    return sorted_training_instances[:k]
 
 
-class HardCodeClassifier:
-    # Professor's Code
-    def __init__(self):
-        pass
-
-    # Professor's Code
-    def fit(self, data, targets):
-        return HardCodedModel();
-
-    # MINE CODE
-    # def fit(self, data, target):
-    #     hc = HardCodeClassifier()
-    #     return hc
-    #
-    # def predict(self, data_test):
-    #     array = np.array([])
-    #     for item in data_test:
-    #         array = np.append(array,0)
-    #     return array
+def _get_tuple_distance(training_instance, test_instance):
+    return (training_instance, get_distance(test_instance, training_instance[0]))
 
 
-# PROFESSOR CODE
-classifierMine = HardCodeClassifier()
-model = classifier.fit(data_train, targets_train)
-targets_predicted = model.predict(data_test)
+def get_distance(data1, data2):
+    points = list(zip(data1, data2))
+    diffs_squared_distance = [pow(a - b, 2) for (a, b) in points]
+    return math.sqrt(sum(diffs_squared_distance))
 
-# Compute the amount we got correct
-correct = (targets_test == targets_predicted).sum()
-total = len(targets_test)
-percent = correct / total * 100
 
-# Display result
-print("Correct: {}/{} or {:.2f}%".format(correct, total, percent))
+def get_majority_vote(neighbours):
+    # index 1 is the class
+    classes = [neighbour[1] for neighbour in neighbours]
+    count = Counter(classes)
+    return count.most_common()[0][0]
 
-# Send the model back
-#return model
 
-# MINE CODE
-# modelMine = classifierMine.fit(data_train, targets_train)
-# targets_predictedMine = modelMine.predict(data_test)
-# print("\nPrinting Mine HardCodeClassifier")
-# print(targets_predictedMine)
-# print(targets_test)
-# comparison = metrics.accuracy_score(targets_test, targets_predictedMine)
-# print("{:.3f}%".format(comparison * 100))
-
+for i in range(len(data_test)):
+    neighbours = get_neighbours(training_set=train, test_instance=test[i][0], k=5)
+    majority_vote = get_majority_vote(neighbours)
+    arrayPredictions.append(majority_vote)
 
 
 # Implementing KNeighbors
-k_range = range(1,50)
+k_range = range(1,4)
 scores = []
 for k in k_range:
     classifier = KNeighborsClassifier(n_neighbors=k)
@@ -119,13 +74,19 @@ for k in k_range:
     scores.append(metrics.accuracy_score(targets_test, predictions))
     print();
     print("Printing KNeighbors K= {} ".format(k))
-    comparison = metrics.accuracy_score(targets_test, predictions)
-    print("{:.3f}%".format(comparison * 100))
+    comparisonKNNAlgo = metrics.accuracy_score(targets_test, predictions)
+    comparisonMineAlgo = metrics.accuracy_score(targets_test, arrayPredictions)
+    
+    print("KNN Algo  = {:.3f}%".format(comparisonKNNAlgo * 100))
+    print("Mine Algo = {:.3f}%".format(comparisonMineAlgo * 100))
+
+print()
+print("ComparisonKNNAlgo : " + str(comparisonKNNAlgo))
+print("ComparisonMineAlgo : " + str(comparisonMineAlgo))
+
 
 # Generating graphs to compare the best K number to use
 plt.plot(k_range, scores)
-plt.xlabel('Value of K for KN')
-plt.ylabel('Testing Accuracy')
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Testing Accuracy for KNN in %')
 plt.show()
-
-
